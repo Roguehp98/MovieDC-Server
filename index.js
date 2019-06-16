3ac4076a889585d7f0907fda9c480e784f09a4fc
Roguehp98 <vdduong14@gmail.com>
2019-06-16 17:29:35 +0700

    test bug

--- index.js
+++ index.js
@@ -36,12 +36,7 @@ const corOptions = {
 //change in Playground setting: "request.credentials": "include"
 server.express.use(cors(corOptions));
 server.express.use(function(req, res, next) {
-  console.log(req.headers.origin)
-  // if (req.headers.origin) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  // }
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
